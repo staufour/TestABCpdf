3d3585297b72f0e17b408c1c4b1213b7cbc774b2
Sylvain Taufour <staufour@talentsoft.com>
2017-08-28 11:22:52 +0200

    Initial commit

---
+++ TestABCpdf/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using WebSupergoo.ABCpdf10;
+
+namespace TestABCpdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!XSettings.InstallLicense("<license_key>"))
+            {
+                throw new Exception("License installation failed.");
+            }
+
+            var currentPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var pdfPath = Path.Combine(currentPath, "doc.pdf");
+            var outputPath = Path.Combine(currentPath, "output\\result.html");
+            var outputFolder = Path.GetDirectoryName(outputPath);
+
+            if (!Directory.Exists(outputFolder))
+            {
+                Directory.CreateDirectory(outputFolder);
+            }
+
+            using (var doc = new Doc())
+            {
+                doc.SetInfo(0, "MSHtmlBootstrap", 0);
+
+                doc.Read(pdfPath);
+                doc.SaveOptions.Folder = "subfolder";
+                doc.Save(outputPath);
+            }
+        }
+    }
+}
