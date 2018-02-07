eebdc11040263e993e92a90dfe31c919bc4dacc3
Sylvain Taufour <4014315+staufour@users.noreply.github.com>
2018-02-07 14:31:15 +0100

    Add a web project and a HTML to PDF conversion

--- TestABCpdf/Program.cs
+++ TestABCpdf/Program.cs
@@ -14,6 +14,74 @@ namespace TestABCpdf
                 throw new Exception("License installation failed.");
             }
 
+            Console.WriteLine("1. Export PDF to HTML. (default)");
+            Console.WriteLine("2. Export HTML to PDF.");
+            var result = Console.ReadLine();
+
+            switch (result)
+            {
+                case "2":
+                    ExportHtmlToPdf();
+                    break;
+                default:
+                    ExportPdfToHtml();
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void ExportHtmlToPdf()
+        {
+            var currentPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var htmlPath = Path.Combine(currentPath, "template.html");
+            var logoPath = Path.Combine(currentPath, "logo.png");
+            var outputPath = Path.Combine(currentPath, "output\\result.pdf");
+            var outputFolder = Path.GetDirectoryName(outputPath);
+
+            var tempFolder = CreateTempFolder();
+            var tempLogoPath = Path.Combine(tempFolder, "logo.png");
+            File.Copy(logoPath, tempLogoPath);
+
+            if (!Directory.Exists(outputFolder))
+            {
+                Directory.CreateDirectory(outputFolder);
+            }
+
+            var content = File.ReadAllText(htmlPath).Replace("$image_path$", new Uri(tempLogoPath).ToString());
+
+            using (var doc = new Doc())
+            {
+                doc.Page = doc.AddPage();
+
+                var id = doc.AddImageHtml(content);
+
+                while (true)
+                {
+                    if (!doc.Chainable(id))
+                    {
+                        break;
+                    }
+
+                    doc.Page = doc.AddPage();
+                    id = doc.AddImageToChain(id);
+                }
+
+                for (int i = 1; i <= doc.PageCount; i++)
+                {
+                    doc.PageNumber = i;
+                    doc.Flatten();
+                }
+
+                doc.Save(outputPath);
+            }
+
+            Console.WriteLine($"PDF path : {outputPath}");
+            Directory.Delete(tempFolder, true);
+        }
+
+        private static void ExportPdfToHtml()
+        {
             var currentPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             var pdfPath = Path.Combine(currentPath, "doc.pdf");
             var outputPath = Path.Combine(currentPath, "output\\result.html");
@@ -26,12 +94,32 @@ namespace TestABCpdf
 
             using (var doc = new Doc())
             {
-                doc.SetInfo(0, "MSHtmlBootstrap", 0);
-
                 doc.Read(pdfPath);
                 doc.SaveOptions.Folder = "subfolder";
                 doc.Save(outputPath);
             }
+
+            Console.WriteLine($"HTML path : {outputPath}");
+        }
+
+        private static string CreateTempFolder()
+        {
+            var fullBaseTempDir = Path.Combine(Path.GetTempPath(), "DocToHtml Temporary Files");
+
+            if (!Directory.Exists(fullBaseTempDir))
+            {
+                Directory.CreateDirectory(fullBaseTempDir);
+            }
+
+            var simpleFileDir = Path.GetRandomFileName();
+            var fileDir = Path.Combine(fullBaseTempDir, simpleFileDir);
+
+            if (!Directory.Exists(fileDir))
+            {
+                Directory.CreateDirectory(fileDir);
+            }
+
+            return fileDir;
         }
     }
 }
---
+++ WebTest/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿namespace WebTest.Controllers
+{
+    using System;
+    using System.IO;
+    using System.Web.Mvc;
+    using WebSupergoo.ABCpdf11;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (!XSettings.InstallLicense("<license_key>"))
+            {
+                throw new Exception("License installation failed.");
+            }
+
+            byte[] pdfContent;
+
+            using (var ms = new MemoryStream())
+            {
+                ExportHtmlToPdf(ms);
+                pdfContent = ms.ToArray();
+            }
+            
+            return File(pdfContent, "application/pdf", "test.pdf");
+        }
+
+        private void ExportHtmlToPdf(Stream outputStream)
+        {
+            var htmlPath = Server.MapPath("~/App_Data/template.html");
+            var logoPath = Server.MapPath("~/App_Data/logo.png");
+
+            var tempFolder = CreateTempFolder();
+            var tempLogoPath = Path.Combine(tempFolder, "logo.png");
+            System.IO.File.Copy(logoPath, tempLogoPath);
+
+            var content = System.IO.File.ReadAllText(htmlPath).Replace("$image_path$", new Uri(tempLogoPath).ToString());
+
+            using (var doc = new Doc())
+            {
+                doc.Page = doc.AddPage();
+
+                var id = doc.AddImageHtml(content);
+
+                while (true)
+                {
+                    if (!doc.Chainable(id))
+                    {
+                        break;
+                    }
+
+                    doc.Page = doc.AddPage();
+                    id = doc.AddImageToChain(id);
+                }
+
+                for (int i = 1; i <= doc.PageCount; i++)
+                {
+                    doc.PageNumber = i;
+                    doc.Flatten();
+                }
+
+                doc.Save(outputStream);
+            }
+
+            Directory.Delete(tempFolder, true);
+        }
+
+        private string CreateTempFolder()
+        {
+            var fullBaseTempDir = Path.Combine(Path.GetTempPath(), "DocToHtml Temporary Files");
+
+            if (!Directory.Exists(fullBaseTempDir))
+            {
+                Directory.CreateDirectory(fullBaseTempDir);
+            }
+
+            var simpleFileDir = Path.GetRandomFileName();
+            var fileDir = Path.Combine(fullBaseTempDir, simpleFileDir);
+
+            if (!Directory.Exists(fileDir))
+            {
+                Directory.CreateDirectory(fileDir);
+            }
+
+            return fileDir;
+        }
+    }
+}
